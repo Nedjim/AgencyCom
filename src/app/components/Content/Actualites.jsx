e4cb817ac906942d0798646a91a6910af2250d48
Nedjim <nedjim.danimon@gmail.com>
2017-03-06 11:57:21 +0100

    Add presentation content

---
+++ src/app/components/Content/Actualites.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Actualites extends React.Component {
+    render(){
+        return (
+            <div id='actualites'>
+                actualites
+            </div>
+        )
+    }
+} 
--- src/app/components/Content/Content.jsx
+++ src/app/components/Content/Content.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 export default class Content extends React.Component {
     render(){
         return (
-            <div>
-                Content
+            <div id='content'>
+                <h1>PLUS D'ENERGIE VITALE <br/>POUR ENTRER DANS LA VIE DE NOS CLIENTS</h1>
+                <div className='description'>
+                   <p> Preferendum met à votre disposition des équipes expertes pour faire battre le coeur de votre Marque à chaque <br/>
+                   étape de ses vies publicitaire, opérationnel, digitale et média...</p>
+                   <p>Preferendum est une agence de communication qui accompagnera votre marque pas à pas tout au long de ses <br/>
+                   différentes vies pour la faire connaître, grandir, évoluer, se moderniser, renaître... </p>
+                </div>
             </div>
         )
     }
