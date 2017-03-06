08d23752f742ca6d1c333bd213c0358deb287dfc
Nedjim <nedjim.danimon@gmail.com>
2017-03-06 11:36:48 +0100

    Add header + banner

--- src/app/components/Banner/Banner.jsx
+++ src/app/components/Banner/Banner.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default class Banner extends React.Component {
     render(){
         return (
-            <div>
-                Banner
+            <div id="banner">
+                <p><span className='font-bold'>BESOIN D'UNE AGENCE</span><br />POUR GERER TOUTES LES VIES <br />DE VOTRE MARQUE?</p>
             </div>
         )
     }
--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -4,7 +4,22 @@ export default class Header extends React.Component {
     render(){
         return (
             <header>
-                Header
+                <img src="app/img/logo.png" id='logo' className='inline-block'/>
+                <div className='inline-block aside'>
+                    <div className='social-network'>
+                        <img src="app/img/fbk.png"/>
+                        <img src="app/img/twitter.png"/>
+                        <img src="app/img/linkedin.png"/>
+                    </div>
+                    <nav>
+                        <li>ACCUEIL</li>
+                        <li>AGENCE</li>
+                        <li>REFERENCE</li>
+                        <li>INTERNATIONAL</li>
+                        <li>BLOG</li>
+                        <li>CONTACT</li>
+                    </nav>
+                </div>
             </header>
         )
     }
