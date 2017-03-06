f0726ac7c95fe998cfa4f18a65efc115406adde9
Nedjim <nedjim.danimon@gmail.com>
2017-03-06 10:40:23 +0100

    Add structure

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Header from './Header/Header.jsx';
+import Banner from './Banner/Banner.jsx';
+import Content from './Content/Content.jsx';
+import Footer from './Footer/Footer.jsx';
 
-export default class Content extends React.Component {
+export default class App extends React.Component {
     render() {
         return (
             <div>
-                Coucou
+                <Header />
+                <Banner />
+                <Content />
+                <Footer />
             </div>
         )
     }
